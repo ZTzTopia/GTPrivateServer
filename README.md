0acde6e969720b40d1b2b7b733df92f7a74f9c0f
ZTzTopia <zentay36@gmail.com>
2022-03-22 18:17:31 +0700

    forget to commit unversioned file

---
+++ README.md
@@ -0,0 +1,42 @@
+# Growtopia Private Server
+This is a private server for Growtopia.
+
+## 📜 Features
+  - [x] Server gateway (This is to connect to sub-servers it will use load balancer to choose which sub-servers are suitable to join).
+  - [x] Load balancer (Weighted least connection algorithm and check load avg for linux).
+  - [x] Sub-server (This is vertical scaling, if u have 4 cpu cores it will run 4 sub-servers. I will make horizontal scaling soon).
+  - [x] Database (MariaDB server or you can use MySql server).
+  - [ ] Anti cheat.
+  - [x] Player database. (Guest)
+  - [ ] Player inventory.
+  - [ ] Player stats.
+  - [ ] Player clothes.
+  - [ ] Player commands.
+  - [x] World database (Will check hash of all tile before saving the world to database).
+  - [x] World block.
+  - [ ] World object.
+  - [ ] World plant.
+  - [ ] World sub-server (Will check load avg before join the world).
+  - [x] Punch block.
+  - [ ] Place block.
+  - [ ] Drop item.
+  - [ ] Pick up item.
+
+## ⚙️ Setting Up
+  - Rename `src/config.sample.h` to `src/config.h` and fill it.
+
+## 🔨 Building The Source
+```shell
+mkdir build
+cd build
+cmake ..
+cmake --build .
+```
+
+## 📦 Running The Source
+  - If you on windows, you need to install mariadb connector c from https://mariadb.com/downloads/connectors/
+  - Move all needed library from `_deps` to `path/to/program`.
+  - Move your latest `items.dat` to `path/to/program/data/items.dat`.
+
+## ✨ Credits
+  - Thanks to one of my friends helped a lot to this project.
---
+++ src/config.sample.h
@@ -0,0 +1,45 @@
+#pragma once
+#include <string>
+
+namespace config {
+    constexpr auto debug = true;
+    constexpr std::string_view address = "127.0.0.1";
+
+    namespace server_gateway {
+        constexpr auto count = 2;
+        constexpr auto port = 17256;
+        constexpr auto max_peer = 32;
+    }
+
+    namespace server_game {
+        constexpr auto count = 4;
+        constexpr auto port = server_gateway::port - (server_game::count + 1);
+        constexpr auto max_peer = 32;
+    }
+
+    namespace database {
+        constexpr std::string_view host = "127.0.0.1";
+        constexpr auto port = 3306;
+        constexpr std::string_view user = "root";
+        constexpr std::string_view password = "";
+        constexpr std::string_view database = "gtps";
+        constexpr auto auto_reconnect = true;
+        constexpr auto debug = false;
+    }
+
+    namespace cdn {
+        constexpr std::string_view host = "ubistatic-a.akamaihd.net";
+        constexpr std::string_view cache = "0098/32621/cache/";
+    }
+
+    namespace on_super_main {
+        constexpr auto param2 = cdn::host;
+        constexpr auto param3 = cdn::cache;
+        constexpr std::string_view param4 = "cc.cz.madkite.freedom org.aqua.gg idv.aqua.bulldog com.cih.gamecih2 com.cih.gamecih com.cih.game_cih cn.maocai.gamekiller com.gmd.speedtime org.dax.attack com.x0.strai.frep com.x0.strai.free org.cheatengine.cegui org.sbtools.gamehack com.skgames.traffikrider org.sbtoods.gamehaca com.skype.ralder org.cheatengine.cegui.xx.multi1458919170111 com.prohiro.macro me.autotouch.autotouch com.cygery.repetitouch.free com.cygery.repetitouch.pro com.proziro.zacro com.slash.gamebuster";
+        constexpr std::string_view param5 = "proto=158|choosemusic=audio/mp3/about_theme.mp3|active_holiday=2|wing_week_day=0|ubi_week_day=0|server_tick=83430103|clash_active=1|drop_lavacheck_faster=1|isPayingUser=0|usingStoreNavigation=1|enableInventoryTab=1|bigBackpack=1|";
+    }
+
+    namespace data {
+        constexpr std::string_view root = "./data/";
+    }
+}
